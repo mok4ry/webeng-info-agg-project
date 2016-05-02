306432bd1237cce68a8ac1c8cf2fcc33276cf68f
Matt Mokary <mxm6060@rit.edu>
2016-05-01 21:39:28 -0400

    disable deselecting repo if it is the last one selected

--- app/js/reducers/sourceFilters.js
+++ app/js/reducers/sourceFilters.js
@@ -1,11 +1,21 @@
 import { SELECT_SOURCE, DESELECT_SOURCE } from '../actions/sourceFilters';
 
+function numSelected(state) {
+  let count = 0;
+  Object.keys(state).forEach(f => {
+    if (state[f]) count++;
+  });
+  return count;
+}
+
 export default function sourceFilters(state = {}, action) {
   switch (action.type) {
     case SELECT_SOURCE:
       return Object.assign({}, state, { [action.source]: true });
 
     case DESELECT_SOURCE:
+      if (numSelected(state) === 1)
+        return state;
       return Object.assign({}, state, { [action.source]: false });
 
     default:
